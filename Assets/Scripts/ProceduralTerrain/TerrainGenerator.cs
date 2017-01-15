b4c9aefe1c634a6bd08edd924a50e6f28ba52f2c
rtracey <robert.w.tracey@gmail.com>
2017-01-15 10:49:28 -0400

    Added reset to clear height changes

--- Assets/Scripts/ProceduralTerrain/TerrainGenerator.cs
+++ Assets/Scripts/ProceduralTerrain/TerrainGenerator.cs
@@ -7,6 +7,7 @@ using System.Collections.Generic;
 public class TerrainGenerator : MonoBehaviour {
 
 	public bool m_ApplyPerlinNoise = false;
+	private bool m_Reset = true;
 
 	public float m_Frequency;
 	public float m_Seed;
@@ -36,32 +37,56 @@ public class TerrainGenerator : MonoBehaviour {
 		
 	void OnValidate()
 	{
-		UpdateTerrain ();
+		if (m_ApplyPerlinNoise) {
+			UpdateTerrain ();
+			m_Reset = false;
+		} else if (!m_Reset) {
+			ResetTerrain ();
+			m_Reset = true;
+		}
 	}
 
 	public void UpdateTerrain()
 	{
 		if (m_Terrain == null || m_Verts == null) {
 			Init ();
-		} else {
+		} 
+
+		if (m_Terrain != null && m_Verts != null) {
 			float vertHeight = 0.0f;
 			m_Verts.Clear ();
 
 			for (float i = 0; i < height; i++) {
 				for (float j = 0; j < width; j++) {
 
-					if (m_ApplyPerlinNoise) {
-						vertHeight = Mathf.PerlinNoise (
-							(j / (float)width) * m_Frequency + m_Seed,
-							(i / (float)height) * m_Frequency + m_Seed);
+					vertHeight = Mathf.PerlinNoise (
+						(j / (float)width) * m_Frequency + m_Seed,
+						(i / (float)height) * m_Frequency + m_Seed);
 
-						vertHeight = 2 * vertHeight - 1;
-						vertHeight *= m_Scale;
-					}
-					else
-						vertHeight = 0.0f;
+					vertHeight = 2 * vertHeight - 1;
+					vertHeight *= m_Scale;
 				
 					m_Verts.Add (new Vector3 (i, vertHeight, j));
+				}
+			}
+
+			m_Terrain.SetVertices (m_Verts);
+			m_Terrain.RecalculateNormals ();
+		}
+	}
+
+	public void ResetTerrain()
+	{
+		if (m_Terrain == null || m_Verts == null) {
+			Init ();
+		} 
+
+		if (m_Terrain != null && m_Verts != null) {
+			m_Verts.Clear ();
+
+			for (float i = 0; i < height; i++) {
+				for (float j = 0; j < width; j++) {
+					m_Verts.Add (new Vector3 (i, 0, j));
 				}
 			}
 
