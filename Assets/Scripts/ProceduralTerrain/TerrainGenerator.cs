d9c71150be15876f1a84021ca242bf9960a09787
rtracey <robert.w.tracey@gmail.com>
2017-01-15 10:43:11 -0400

    Clean up

--- Assets/Scripts/ProceduralTerrain/TerrainGenerator.cs
+++ Assets/Scripts/ProceduralTerrain/TerrainGenerator.cs
@@ -7,53 +7,66 @@ using System.Collections.Generic;
 public class TerrainGenerator : MonoBehaviour {
 
 	public bool m_ApplyPerlinNoise = false;
-	public bool m_ApplyDeltaTime = false;
 
-	public float m_PerlinFrequency;
-	public float m_PerlinSeed;
-	public float m_PerlinScale;
+	public float m_Frequency;
+	public float m_Seed;
+	public float m_Scale;
 
+	[HideInInspector]
 	public int width, height;
 
 	private Mesh m_Terrain;
 	private List<Vector3> m_Verts;
 
+	void Start()
+	{
+		Init ();
+	}
+
 	void OnEnable()
+	{
+		Init ();
+	}
+
+	private void Init()
 	{
 		m_Terrain = GetComponent<MeshFilter> ().sharedMesh;
 		m_Verts = new List<Vector3> (width * height);
 	}
-
-	void Update()
-	{
-		if(m_ApplyDeltaTime)
-			UpdateTerrain(Time.deltaTime);
-	}
-
+		
 	void OnValidate()
 	{
-		UpdateTerrain (1.0f);
+		UpdateTerrain ();
 	}
 
-	public void UpdateTerrain(float delta)
+	public void UpdateTerrain()
 	{
-		float vertHeight = 0.0f;
-		m_Verts.Clear ();
+		if (m_Terrain == null || m_Verts == null) {
+			Init ();
+		} else {
+			float vertHeight = 0.0f;
+			m_Verts.Clear ();
 
-		for (float i = 0; i < height; i++) {
-			for (float j = 0; j < width; j++) {
+			for (float i = 0; i < height; i++) {
+				for (float j = 0; j < width; j++) {
 
-				if (m_ApplyPerlinNoise)
-					vertHeight = (2 * Mathf.PerlinNoise ((j/(float)width) * m_PerlinFrequency + m_PerlinSeed,
-						(i/(float)height) * m_PerlinFrequency + m_PerlinSeed) - 1) * m_PerlinScale;
-				else
-					vertHeight = 0.0f;
+					if (m_ApplyPerlinNoise) {
+						vertHeight = Mathf.PerlinNoise (
+							(j / (float)width) * m_Frequency + m_Seed,
+							(i / (float)height) * m_Frequency + m_Seed);
+
+						vertHeight = 2 * vertHeight - 1;
+						vertHeight *= m_Scale;
+					}
+					else
+						vertHeight = 0.0f;
 				
-				m_Verts.Add (new Vector3 (i, vertHeight, j));
+					m_Verts.Add (new Vector3 (i, vertHeight, j));
+				}
 			}
-		}
 
-		m_Terrain.SetVertices (m_Verts);
-		m_Terrain.RecalculateNormals ();
+			m_Terrain.SetVertices (m_Verts);
+			m_Terrain.RecalculateNormals ();
+		}
 	}
 }
